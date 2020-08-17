c2e4b507ecee7c217591561c028f25a40d552f1e
abcdefg <choiyurim2@naver.com>
2020-08-17 23:44:29 +0900

    SecurityConfig 추가

---
+++ src/main/java/simplebookrestapi/infra/config/SecurityConfig.java
@@ -0,0 +1,53 @@
+package simplebookrestapi.infra.config;
+
+import org.springframework.boot.autoconfigure.security.oauth2.resource.OAuth2ResourceServerProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+
+import lombok.RequiredArgsConstructor;
+import simplebookrestapi.infra.clamins.OrganizationSubClaimAdapter;
+import simplebookrestapi.modules.service.AccountService;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	
+	private final AccountService accountService;
+	//private final AppProperties appProperties;
+	private final PasswordEncoder passwordEncoder;
+	
+	@Bean
+	@Override
+	protected AuthenticationManager authenticationManager() throws Exception {
+		// TODO Auto-generated method stub
+		return super.authenticationManager();
+	}
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.userDetailsService(accountService).passwordEncoder(passwordEncoder);
+		auth.authenticationProvider(authenticationProvider());
+	}
+	
+	@Bean
+	public DaoAuthenticationProvider authenticationProvider() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(accountService);
+		authenticationProvider.setPasswordEncoder(passwordEncoder); // 패스워드를 암호활 경우 사용한다
+		return authenticationProvider;
+	}
+		
+}
